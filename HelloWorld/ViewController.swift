ef01e64954e0358b5bd199e7095cd44b6f867d7b
Bellaaspera <lanasenatorova@gmail.com>
2022-06-17 19:43:09 +0300

    Main screen

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Светлана Сенаторова on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
